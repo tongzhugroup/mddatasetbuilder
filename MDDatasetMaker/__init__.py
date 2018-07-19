5907b6e3aa143a511ebcee5764894886e5c500f4
Jinzhe Zeng <njzjz@qq.com>
2018-07-19 23:26:07 +0800

    Change methods.

--- MDDatasetMaker/__init__.py
+++ MDDatasetMaker/__init__.py
@@ -4,10 +4,11 @@
 import itertools
 import numpy as np
 import os
+import shutil
 from ReacNetGenerator import ReacNetGenerator
 
 class DatasetMaker(object):
-    def __init__(self,n_eachspecies=10,bondfilename="bonds.reaxc",dumpfilename="dump.ch4",moleculefilename=None,tempfilename=None,dataset_dir="dataset",xyzfilename="md"):
+    def __init__(self,atomname=["C","H","O"],clusteratom=["C","H","O"],n_eachspecies=10,bondfilename="bonds.reaxc",dumpfilename="dump.ch4",moleculefilename=None,tempfilename=None,dataset_dir="dataset",xyzfilename="md"):
         print("MDDatasetMaker")
         print("Author: Jinzhe Zeng")
         print("Email: njzjz@qq.com 10154601140@stu.ecnu.edu.cn")
@@ -18,46 +19,81 @@ class DatasetMaker(object):
         self.dataset_dir=dataset_dir
         self.xyzfilename=xyzfilename
         self.n_eachspecies=n_eachspecies
+        self.atomname=np.array(atomname)
+        self.clusteratom=clusteratom
         self.dataset={}
-        self.ReacNetGenerator=ReacNetGenerator(runHMM=False,inputfilename=self.bondfilename,moleculefilename=self.moleculefilename,moleculetemp2filename=self.tempfilename)
+        self.atombondtype=[]
+        self.trajatom_dir="trajatom"
+        self.ReacNetGenerator=ReacNetGenerator(atomname=self.atomname,runHMM=False,inputfilename=self.bondfilename,moleculefilename=self.moleculefilename,moleculetemp2filename=self.tempfilename)
+        self.nuclearcharge={"H":1,"He":2,"Li":3,"Be":4,"B":5,"C":6,"N":7,"O":8,"F":9,"Ne":10}
 
     def makedataset(self,processtraj=True):
         if processtraj:
+            self.ReacNetGenerator.inputfilename=self.bondfilename
             self.ReacNetGenerator.step1()
             self.ReacNetGenerator.step2()
             if self.ReacNetGenerator.SMILES:
                 self.ReacNetGenerator.printmoleculeSMILESname()
             else:
                 self.ReacNetGenerator.printmoleculename()
-        self.readmoname()
-        self.writexyzfile()
+        self.readlammpscrdN()
+        #self.readmoname()
+        #self.sorttrajatoms()
+        self.writecoulumbmatrixs()
 
     def readmoname(self):
-        datasets={}
+        if os.path.exists(self.trajatom_dir):
+            shutil.rmtree(self.trajatom_dir)
+        os.makedirs(self.trajatom_dir)
         with open(self.moleculefilename) as fm,open(self.tempfilename) as ft:
             for linem,linet in zip(fm,ft):
                 sm=linem.split()
                 st=linet.split()
                 mname=sm[0]
-                matoms=np.array([int(x)-1 for x in sm[1].split(",")])
-                steps=np.array([int(x) for x in st[-1].split(",")])
+                matoms=np.array([int(x) for x in sm[1].split(",")])
+                mbonds=np.array([[int(y) for y in x.split(",")] for x in sm[2].split(";")]) if len(sm)==3 else np.array([])
+                for atom in matoms:
+                    if self.atomname[self.atomtype[atom]-1] in self.clusteratom:
+                        atombond=[]
+                        for bond in mbonds:
+                            if bond[0]==atom or bond[1]==atom:
+                                atombond.append(bond[2])
+                        atombondstr="".join(str(x) for x in sorted(atombond))
+                        if not self.atomname[self.atomtype[atom]-1]+atombondstr in self.atombondtype:
+                            self.atombondtype.append(self.atomname[self.atomtype[atom]-1]+atombondstr)
+                        with open(self.trajatom_dir+"/trajatom."+self.atomname[self.atomtype[atom]-1]+atombondstr,"a") as f:
+                            print(atom,st[-1],file=f)
+        with open("trajatom.list","w") as f:
+            for atombondstr in  self.atombondtype:
+                print(atombondstr,file=f)
+
+    def sorttrajatom(self,trajatomfilename):
+        dstep={}
+        with open(self.trajatom_dir+"/trajatom."+trajatomfilename) as f:
+            for line in f:
+                s=line.split()
+                steps=np.array([int(x) for x in s[-1].split(",")])
                 for step in steps:
-                    if mname in datasets:
-                        if datasets[mname][0]+1<self.n_eachspecies:
-                            datasets[mname][1].append((step,matoms))
-                        else:
-                            r=np.random.randint(0,datasets[mname][0]+1)
-                            if r<=self.n_eachspecies-1:
-                                datasets[mname][1][r]=(step,matoms)
-                        datasets[mname][0]+=1
+                    if step in dstep:
+                        dstep[step].append(s[0])
                     else:
-                        datasets[mname]=[0,[(step,matoms)]]
-        for spec in datasets.values():
-            for struc in spec[1]:
-                if struc[0] in self.dataset:
-                    self.dataset[struc[0]].append(struc[1])
-                else:
-                    self.dataset[struc[0]]=[struc[1]]
+                        dstep[step]=[s[0]]
+        with open(self.trajatom_dir+"/stepatom."+trajatomfilename,"w") as f:
+            for step,atoms in dstep.items():
+                print(step,",".join(atoms),file=f)
+
+    def trajlist(self):
+        with open("trajatom.list") as f:
+            for line in f:
+                yield line
+
+    def sorttrajatoms(self):
+        for line in self.trajlist():
+            self.sorttrajatom(line.strip())
+
+    def writecoulumbmatrixs(self):
+        for line in self.trajlist():
+            self.writecoulumbmatrix(line.strip())
 
     def readlammpscrdstep(self,item):
         (step,lines),_=item
@@ -85,22 +121,36 @@ class DatasetMaker(object):
                         boxsize.append(float(s[1])-float(s[0]))
         return atomtype,atomcrd,np.array(boxsize)
 
-    def writexyzfile(self):
-        i=0
+    def readlammpscrdN(self):
         self.ReacNetGenerator.inputfilename=self.dumpfilename
-        steplinenum=self.ReacNetGenerator.readlammpscrdN()
-        if not os.path.exists(self.dataset_dir):
-            os.makedirs(self.dataset_dir)
-        with open(self.dumpfilename) as f:
-            for item in enumerate(itertools.islice(itertools.zip_longest(*[f]*steplinenum),0,None,1)):
-                if item[0] in self.dataset:
+        self.steplinenum=self.ReacNetGenerator.readlammpscrdN()
+        self.N=self.ReacNetGenerator.N
+        self.atomtype=self.ReacNetGenerator.atomtype
+
+    def writecoulumbmatrix(self,trajatomfilename,cutoff=3.5):
+        dstep={}
+        with open(self.trajatom_dir+"/stepatom."+trajatomfilename) as f:
+            for line in f:
+                s=line.split()
+                dstep[int(s[0])]=[int(x) for x in s[1].split(",")]
+        with open(self.dumpfilename) as f,open(self.trajatom_dir+"/coulumbmatrix."+trajatomfilename,'w') as fm:
+            for item in enumerate(itertools.islice(itertools.zip_longest(*[f]*self.steplinenum),0,None,1)):
+                if item[0] in dstep:
                     atomtype,atomcrd,boxsize=self.readlammpscrdstep((item,None))
-                    for struc in self.dataset[item[0]]:
-                        struccrd=atomcrd[struc]
-                        for j in range(1,len(struccrd)):
-                            struccrd[j]-=np.round((struccrd[j]-struccrd[0])/boxsize)*boxsize
-                        self.ReacNetGenerator.convertxyz(atomtype[struc],struccrd,self.dataset_dir+"/"+self.xyzfilename+"_"+str(i)+".xyz")
-                        i+=1
+                    for atoma in dstep[item[0]]:
+                        cutoffatoms=[]
+                        for i in range(len(atomcrd)):
+                            dxyz=atomcrd[atoma]-atomcrd[i]
+                            dxyz=dxyz-np.round(dxyz/boxsize)*boxsize
+                            if 0<np.linalg.norm(dxyz)<=cutoff:
+                                cutoffatoms.append(i)
+                        cutoffcrds=atomcrd[cutoffatoms]
+                        for j in range(1,len(cutoffcrds)):
+                            cutoffcrds[j]-=np.round((cutoffcrds[j]-atomcrd[i])/boxsize)*boxsize
+                        print(item[0],atoma,",".join(str(x) for x in self.calcoulumbmatrix(atomtype[atoma],atomcrd[atoma],atomtype[cutoffatoms],cutoffcrds)),file=fm)
+
+    def calcoulumbmatrix(self,atomtypea,atomcrda,atomtype,atomcrd):
+        return np.sort([self.nuclearcharge[self.atomname[atomtype[i]-1]]/np.linalg.norm(atomcrd[i]-atomcrda) for i in range(len(atomcrd))])
 
 if __name__ == '__main__':
     DatasetMaker(bondfilename="bonds.reaxc.ch4_new",dataset_dir="dataset_ch4",xyzfilename="ch4").makedataset(processtraj=False)
