42ddad4ecc8785eef6d63c161b8fa80654652ce6
Jinzhe Zeng <njzjz@qq.com>
2018-07-25 15:34:57 +0800

    Fix a bug.

--- MDDatasetMaker/__init__.py
+++ MDDatasetMaker/__init__.py
@@ -178,7 +178,7 @@ class DatasetMaker(object):
             atomtype,atomcrd,boxsize=self.readlammpscrdstep(item)
             for atoma in self.dstep[step]:
                 cutoffatoms=[]
-                for i in range(len(atomcrd)):
+                for i in range(1,self.N+1):
                     dxyz=atomcrd[atoma]-atomcrd[i]
                     dxyz=dxyz-np.round(dxyz/boxsize)*boxsize
                     if np.linalg.norm(dxyz)<=self.cutoff:
@@ -270,7 +270,7 @@ class DatasetMaker(object):
             molecules=self.ReacNetGenerator.readlammpsbondstep(((step,bondlines),None))[0].keys()
             for atoma in self.dstep[step]:
                 cutoffatoms=[]
-                for i in range(len(atomcrd)):
+                for i in range(1,N+1):
                     dxyz=atomcrd[atoma]-atomcrd[i]
                     dxyz=dxyz-np.round(dxyz/boxsize)*boxsize
                     if np.linalg.norm(dxyz)<=self.cutoff:
