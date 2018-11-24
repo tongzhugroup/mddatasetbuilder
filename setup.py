c6dbb86f0e6a7316e91a354af180dcf7cba3bd5c
Jinzhe Zeng <njzjz@qq.com>
2018-11-24 15:12:08 +0800

    add version number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name='MDDatasetMaker',
-      version='1.0.6',
+      version='1.0.7',
       description='A script to make molecular dynamics (MD) datasets for neural networks from given LAMMPS trajectories automatically.',
       keywords="molecular dynamics dataset",
       url='https://github.com/njzjz/MDDatasetMaker',
