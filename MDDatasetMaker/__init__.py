8ff555d4d78108c1bd545ca66852226f15e905f9
Jinzhe Zeng <njzjz@qq.com>
2019-01-02 01:51:18 +0800

    bugfix

--- MDDatasetMaker/__init__.py
+++ MDDatasetMaker/__init__.py
@@ -179,7 +179,7 @@ class DatasetMaker(object):
         top = np.outer(atoms.numbers, atoms.numbers).astype(np.float64)
         r = atoms.get_all_distances(mic=True)
         diag = np.array([self._coulumbdiag[symbol]
-                         for symbol in atoms.symbols])
+                         for symbol in atoms.get_chemical_symbols()])
         with np.errstate(divide='ignore', invalid='ignore'):
             np.divide(top, r, top)
             np.fill_diagonal(top, diag)
