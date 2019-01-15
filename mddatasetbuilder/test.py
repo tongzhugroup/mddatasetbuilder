45c45ef89835936cfd6c0d6fa181ee3b91b3d09e
Jinzhe Zeng <njzjz@qq.com>
2019-01-15 19:18:25 +0800

    fix

---
+++ mddatasetbuilder/test.py
@@ -0,0 +1,48 @@
+'''Test 
+python setup.py test
+'''
+
+
+import os
+import unittest
+
+from mddatasetbuilder import DatasetBuilder
+import requests
+
+
+def download_file(url, local_filename):
+    # from https://stackoverflow.com/questions/16694907/how-to-download-large-file-in-python-with-requests-py
+    r = requests.get(url, stream=True)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+    return local_filename
+
+
+class TestReacNetGen(unittest.TestCase):
+    def test_reacnetgen(self):
+        # download bonds.reaxc and dump.reaxc
+        bondfile_url = "https://drive.google.com/uc?authuser=0&id=1CJ22BZTh2Bg3MynHyk_CVZl0rcpSQzRn&export=download"
+        dumpfile_url = "https://drive.google.com/uc?authuser=0&id=1-MZZEpTj71JJn4JfKPh5yb_lD2V7NS-Y&export=download"
+        folder = "test"
+        bondfile = "bonds.reaxc"
+        dumpfile = "dump.reaxc"
+        bondfilename = os.path.join(folder, bondfile)
+        dumpfilename = os.path.join(folder, dumpfile)
+
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        print(f"Downloading {bondfile} ...")
+        download_file(bondfile_url, bondfilename)
+        print(f"Downloading {dumpfile} ...")
+        download_file(dumpfile_url, dumpfilename)
+
+        d=DatasetBuilder(bondfilename=bondfilename,dumpfilename=dumpfilename,atomname=["H","O"],dataset_name="h2")
+        d.builddataset()
+
+        self.assertTrue(os.path.exists(d.gjfdir))
+
+
+if __name__ == '__main__':
+    unittest.main()
