87bc46c8d81c4e3d404cc89a83f3f193069c16eb
Jinzhe Zeng <njzjz@qq.com>
2019-02-21 10:11:13 +0800

    fix: bugfix

--- mddatasetbuilder/datasetbuilder.py
+++ mddatasetbuilder/datasetbuilder.py
@@ -21,7 +21,7 @@ from multiprocessing import Pool, Semaphore, cpu_count
 
 import numpy as np
 import pybase64
-import lz4
+import lz4.frame
 from ase import Atom, Atoms
 from ase.data import atomic_numbers
 from ase.io import write as write_xyz
