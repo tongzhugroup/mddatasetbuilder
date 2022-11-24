281623840abf2a1c9323810ad6ad364177db4418
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-11-24 04:23:24 -0500

    use `exist_ok=True` and remove `_mkdir`

--- tests/test_datasetbuilder.py
+++ tests/test_datasetbuilder.py
@@ -61,10 +61,7 @@ class TestMDDatasetBuilder:
             if os.path.isfile(pathfilename) and self._checksha256(
                     pathfilename, sha256):
                 break
-            try:
-                os.makedirs(os.path.split(pathfilename)[0])
-            except OSError:
-                pass
+            os.makedirs(os.path.split(pathfilename)[0], exist_ok=True)
 
             # from https://stackoverflow.com/questions/16694907
             if not isinstance(urls, list):
