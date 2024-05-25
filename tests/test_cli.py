dbebd5d1e0bdd055eda6e26bc23bb18beceef8d4
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-05-25 18:44:34 -0400

    add docs

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,8 @@
+"""Test command line interface."""
 import subprocess as sp
 import sys
 
 
 def test_module():
+    """Test python -m mddatasetbuilder."""
     sp.check_output([sys.executable, "-m", "mddatasetbuilder", "-h"])
