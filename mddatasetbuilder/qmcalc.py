078e18e0a1fa860d65f0eb5b4952841106539b1b
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-05-25 18:31:52 -0400

    feat: support assigning ncpus for qmcalc
    Fix #196.
    
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- mddatasetbuilder/qmcalc.py
+++ mddatasetbuilder/qmcalc.py
@@ -2,18 +2,19 @@
 
 import argparse
 import os
+from typing import Optional
 
 from gaussianrunner import GaussianRunner
 
 
-def qmcalc(gjfdir, command="g16"):
+def qmcalc(gjfdir, command="g16", cpu_num: Optional[int]=None):
     """QM Calculation."""
     gjflist = [
         os.path.join(gjfdir, filename)
         for filename in os.listdir(gjfdir)
         if filename.endswith(".gjf")
     ]
-    GaussianRunner(command=command).runGaussianInParallel("GJF", gjflist)
+    GaussianRunner(command=command, cpu_num=cpu_num).runGaussianInParallel("GJF", gjflist)
 
 
 def _commandline():
@@ -21,6 +22,9 @@ def _commandline():
     parser.add_argument("-d", "--dir", help="Dataset dirs", required=True)
     parser.add_argument(
         "-c", "--command", help="Gaussian command, default is g16", default="g16"
+    )
+    parser.add_argument(
+        "-n", "--ncpus", help="Number of CPU cores used, if not set, all CPU cores are used", default=None
     )
     args = parser.parse_args()
     qmcalc(gjfdir=args.dir, command=args.command)
